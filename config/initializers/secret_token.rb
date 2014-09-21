e416bb636cafeb92de26e806bacdc931ac31ce26
root <root@bus.zjut.com>
2014-09-21 22:35:16 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewgit::Application.config.secret_token = 'bdda7d80e2934e4c9da459fcfc99b18e7881c46af18edb58ebcc6e72164f225011c0a22b7f21f8d50bb48bcf5299e9571ad950457353b17530c21ed5e8ecca17'
